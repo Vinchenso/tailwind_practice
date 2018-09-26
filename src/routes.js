39eef3c80a4fee156c92285f89c022a9ff0ffaaf
Vinchenso Kendall <chensok@gmail.com>
2018-09-26 18:31:15 +0200

    add Home route and wild card route to Home

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import WPPusher from "./components/wpusher.vue";
 import TailwindComponents from "./components/TailwindComponents.vue";
+import Home from "./components/home.vue";
 
 export const routes = [
+  { path: "/", component: Home },
   { path: "/wp-pusher", component: WPPusher },
-  { path: "/tailwind-components", component: TailwindComponents }
+  { path: "/tailwind-components", component: TailwindComponents },
+  { path: "*", redirect: "/" }
 ];
